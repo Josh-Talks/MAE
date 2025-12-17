a0fecb5d437ce89a4ce44b7f25079c50d81b48ba
Joshua Talks <joshua.talks@embl.de>
2025-12-17 15:00:35 +0100

    add image logging and save checkpoint

--- src/MAE/network/trainer.py
+++ src/MAE/network/trainer.py
@@ -1,16 +1,18 @@
 import math
 from pydantic import BaseModel
 import torch
+import os
 from torch.utils.data import DataLoader
 from torch.amp.grad_scaler import GradScaler
 from torch.amp.autocast_mode import autocast
 from typing import List, Literal, Optional, Tuple, Union
 import wandb
 
+from .logging import get_current_lr, get_logger, RunningAverage, WandbConfig
 from .loss import MSELossPatched
-from .model import MaskedAutoencoder, ModelConfig
 from .lr_sched import adjust_learning_rate
-from .logging import get_current_lr, get_logger, RunningAverage
+from .model import MaskedAutoencoder, ModelConfig
+from .utils import save_checkpoint, unpatchify
 
 from MAE.datasets.dataset import get_pretrain_loader, LoaderConfig
 
@@ -19,6 +21,9 @@ logger = get_logger("MAETrainer")
 
 class LoggingParams(BaseModel):
     log_after_n_iters: int
+    log_images_after_n_iters: int
+    save_nth_ckpt: int
+    ckpt_dir: str
 
 
 class OptimizerParams(BaseModel):
@@ -35,10 +40,12 @@ class TrainingParameters(BaseModel):
     current_epoch: int
     current_iteration: int
     optimizer_params: OptimizerParams
+    device: str = "cuda"
     logging_params: LoggingParams
 
 
 class MAETrainingConfig(BaseModel):
+    logging: WandbConfig
     loss_type: Literal["MSE"]
     optimizer_type: Literal["AdamW"]
     model_params: ModelConfig
@@ -113,9 +120,11 @@ class MAETrainer:
         self.loss_criteria = loss_criteria
         self.scaler = scaler
         self.optimizer = optimizer
+        self.device = training_params.device
 
         self.opt_params = training_params.optimizer_params
         self.lg_params = training_params.logging_params
+        self.ckpt_dir = self.lg_params.ckpt_dir
 
         self.current_iteration = training_params.current_iteration
         self.current_epoch = training_params.current_epoch
@@ -133,9 +142,17 @@ class MAETrainer:
         self.scaler = scaler
 
     def fit(self):
+        _ = self.model.to(self.device)
         for _ in range(self.current_epoch, self.max_num_epochs):
             # train for one epoch
-            should_terminate = self.train()
+            loss, should_terminate = self.train()
+
+            if self.current_epoch % self.lg_params.save_nth_ckpt == 0:
+                ckpt_name = f"ckpt_epoch_{self.current_epoch}.pytorch"
+            else:
+                ckpt_name = None
+
+            self._save_checkpoint(loss, ckpt_name)
 
             if should_terminate:
                 logger.info("Stopping criterion is satisfied. Finishing training")
@@ -155,6 +172,7 @@ class MAETrainer:
         n_batches = len(self.dataloader)
         lr = get_current_lr(self.optimizer)
         wandb.log({"learning-rate": lr}, step=self.current_iteration)
+        loss = torch.tensor(float("inf"))
 
         for x in self.dataloader:
             # adjust learning rate
@@ -174,9 +192,9 @@ class MAETrainer:
                 step=self.current_iteration,
             )
 
-            x = x.to(self.model.device)
+            x = x.to(self.device)
 
-            with autocast("cuda"):
+            with autocast(self.device):
                 pred, mask = self.model(x)
                 loss = self.loss_criteria(pred, x, mask, self.model)
 
@@ -193,17 +211,45 @@ class MAETrainer:
 
                 wandb.log(
                     {
+                        "train_loss": loss.item(),
                         "train_loss_avg": train_losses.avg,
                     },
                     step=self.current_iteration,
                 )
 
+            if self.current_iteration % self.lg_params.log_images_after_n_iters == 0:
+                # Unpatchify prediction to get back to original spatial dimensions
+                pred_img = unpatchify(
+                    pred,
+                    self.model.patch_size,
+                    self.model.img_size,
+                    self.model.in_channels,
+                    self.model.spatial_dims,
+                )
+
+                # Get first image from batch
+                input_img = x[0].detach().cpu().numpy()
+                pred_img = pred_img[0].detach().cpu().numpy()
+
+                # Log side-by-side images
+                wandb.log(
+                    {
+                        "input_target": wandb.Image(
+                            input_img, caption="Input (Target)"
+                        ),
+                        "reconstruction": wandb.Image(
+                            pred_img, caption="Reconstruction"
+                        ),
+                    },
+                    step=self.current_iteration,
+                )
+
             if self.max_num_iterations < self.current_iteration:
-                return True
+                return float(loss.item()), True
 
             self.current_iteration += 1
 
-        return False
+        return float(loss.item()), False
 
     @staticmethod
     def _batch_size(
@@ -213,3 +259,22 @@ class MAETrainer:
             return input[0].size(0)
         else:
             return input.size(0)
+
+    def _save_checkpoint(self, loss: float, ckpt_name: Optional[str] = None):
+
+        state_dict = self.model.state_dict()
+
+        last_file_path = os.path.join(self.ckpt_dir, "last_checkpoint.pytorch")
+        logger.info(f"Saving checkpoint to '{last_file_path}'")
+
+        save_checkpoint(
+            {
+                "num_epochs": self.current_epoch + 1,
+                "num_iterations": self.current_iteration,
+                "model_state_dict": state_dict,
+                "loss": loss,
+                "optimizer_state_dict": self.optimizer.state_dict(),
+            },
+            checkpoint_dir=self.ckpt_dir,
+            checkpoint_name=ckpt_name,
+        )
